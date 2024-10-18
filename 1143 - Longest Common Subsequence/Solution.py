dbaf6624d3f40af63a95b6a790bb68f6edecc8fb
Matt Curschman <mcurschman@conferencecatalysts.com>
2024-10-17 23:24:15 -0400

    fixed some issues and proeprly added 1143

--- 1143 - Longest Common Subsequence/Solution.py
+++ 1143 - Longest Common Subsequence/Solution.py
@@ -1,1 +1,21 @@
-print(hello)
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+        # establish a 2D array of zeros 1 larger than the length of t1 and t2
+        # each position will hold the length of the longest subsequence in the array
+        # by the end, 0,0 will hold the lkongest common subsrq in the entire array
+        # as all the values will have been passed back up to it
+        # dp = [[0 for j in range(len(text2) +1)] for i in range(len(text1)+1)]
+
+        # #go through in reverse order, calculating each value (bottom up)
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                # if the values are equal, get the diagonal value (means they have the same, so sub-prob is both strings minus first char)
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                # if not equal, then we need to reduce one of the strings. get the max of reducing either string, this accounts for subsequ after a non-match
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+        # resutls will bubble back up and to the left, with the final one ending up at [0][0]
+        return dp[0][0]
+
