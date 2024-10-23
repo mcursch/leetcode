8d29000934f261ec85b1be0b56a3a59a5fea3f95
Matt Curschman <mcurschman@conferencecatalysts.com>
2024-10-23 00:30:47 -0400

    renamed files and added some more stuff

--- test.py
+++ test.py
@@ -1,5 +1,32 @@
-def main():
-    print('hi')
+import os
 
-if __name__ == "__main__":
-    main()
+
+
+def traverse_directory(path):
+    for dirpath, dirnames, filenames in os.walk(path):
+        for dirname in dirnames:
+            if '-' in dirname and 'site-packages' not in dirname and 'pip' not in dirname:
+                number = dirname.split(' ')[0]
+                if len(number) < 5:
+                    new_number = number
+                    if int(number) < 100:
+                        new_number = new_number = '000' + number
+                    if 100 <= int(number) < 1000:
+                        new_number = '00' + number
+                    if 1000 <= int(number) < 10000:
+                        new_number = '0' + number
+                    arr_name = dirname.split(' ')
+                    arr_name[0] = new_number
+                    new_name = ' '.join(arr_name)
+                    os.rename(dirname, new_name)
+
+
+
+    # for filename in filenames:
+        #     if '000' in filename:
+        #         pass
+        #     else:
+        #         number = filename.split(' ')
+        #         print(number)
+
+traverse_directory('dirname')
